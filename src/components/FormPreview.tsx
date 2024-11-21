1c0326be6e3069ee8ac30a6bc00f52412bc51787
Varshith Reddy <144758192+varshith5577@users.noreply.github.com>
2024-11-21 13:01:36 +0530

    Update FormPreview.tsx

--- src/components/FormPreview.tsx
+++ src/components/FormPreview.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useForm, RegisterOptions } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { FormSchema, FormField } from "../types/form";
 import { downloadJson } from "../utils/fileUtils";
 
